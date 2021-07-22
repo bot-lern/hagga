b8d26298e48c2e40632264bcd4f821ac58ff4a26
Agent34567 <87294221+Agent34567@users.noreply.github.com>
2021-07-22 11:16:27 +0530

    ok

--- plugins/menu.js
+++ plugins/menu.js
@@ -7,6 +7,7 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'ravana', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
+  var image = await axios.get (Config.MENU_LOGO, {responseType: 'arraybuffer'})
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.MENU_LOGO})
 
