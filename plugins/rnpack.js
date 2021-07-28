26af86403f9c5a6c39f97e55787b99d7d476e9e4
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-07-28 20:19:23 +0530

    Update rnpack.js

--- plugins/rnpack.js
+++ plugins/rnpack.js
@@ -59,7 +59,7 @@ var webimage = await axios.get(`https://api.xteam.xyz/textpro/skeleton?text=${en
         topText = split[0];
 }
 
-    var webimage = await axios.get(`https://api.xteam.xyz/textpro/ultragloss?text=${encodeURIComponent(match[1])}&APIKEY=5bd33b276d41d6b4`, { responseType: 'arraybuffer' })
+    var webimage = await axios.get(`https://api.xteam.xyz/textpro/ultragloss?text=${encodeURIComponent(match[1])}&APIKEY`=Config.XTEAM_API, { responseType: 'arraybuffer' })
 
    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
 
