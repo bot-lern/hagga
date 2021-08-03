7dcd86fd7361e9002eab39bdffb4b4b094cc0fae
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-08-03 08:46:01 +0530

    Rhelp

---
+++ plugins/rhelp.js
@@ -0,0 +1,25 @@
+/* # Exclusively from RAVANA
+# Do not use this fore any commercial thing
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved RAVANA Wa.me/+94714898434
+# Credit : usufusta-whatsappAsena
+*/
+
+const RAVANA  = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+RAVANA.addCommand({pattern: 'rhelp', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*🇱🇰HELLOW THIS IS RAVANA HELP CENTER🇱🇰* \n\n✨What's Your Problem \n\n\n▶සිංදු වීඩියෝ එන්නේ නෑ(.1) \n\n▶ලොගෝ වැඩ නෑ(.2)\n\n▶Alive එක වැඩ කරන්නේ නෑ(.3)\n\n▶Shutdown කරාම on කරන්නේ කොහොමද(.4) \n\n▶බොට් එක පාරම වැඩ නැතුව ගියොත් මොකද කරන්නේ(.5) \n\n▶වෙනත්(.6)");
+
+}));
+RAVANA.addCommand({pattern: '1', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*කරුණාකර ඔබගේ බොට්ව restart කරන්න.* \n\n .restart \n\n🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
+RAVANA.addCommand({pattern: '2', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*එය අපගේ ප්‍රශ්නයක් නොවේ එය ලොගෝ සයිට් එකේ ලිමිට් එක නිසා* \n\nදැන් එයට විසදුමක් ඇත .\n\n```ඔයාලට පුලුවන් ඒ ලොගෝ api වෙනස් කිරීමට ´´´\n\n.setvar XTEAM_API:ඔයාල ගත්ත api එක \n\n .setvar LOLUHUMAN_API:ඔයාල ගත්ත api එක \n\n\nඔය api ගන්න ක්‍රමේ මෙම වීඩියෝ එකේ ඇත.\n https://youtu.be/vGHpome5e3k\n\n\n 🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
