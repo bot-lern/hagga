91191bc2d5c707825aeadb2810ac3b1c36e4ee77
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-08-10 11:42:40 +0530

    https://gist.github.com/Agent34567/23046835c2a218dffbb3e33ae880af35Ara kade kena thibba eka maru karala parana ekak dammada danne nahttps://gist.github.com/Agent34567/23046835c2a218dffbb3e33ae880af35

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -426,7 +426,7 @@ if (config.WORKTYPE == 'private') {
             });
     }));
 
-@@ -785,7 +819,41 @@ else if (config.WORKTYPE == 'public') {
+@@ -785,else if (config.WORKTYPE == 'public') {
                 writer.addTag();
 
                 reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG,MessageType.text);
